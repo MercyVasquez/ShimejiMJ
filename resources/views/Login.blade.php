f88a11c63e3ef3e4a441a72d7c90f51341155621
Mercy <68203696+MercyVasquez@users.noreply.github.com>
2021-12-03 07:13:51 -0300

    CuentaUsuario

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -5,15 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <link rel="stylesheet" href="{{asset('css/registro.css')}}">
-    <link rel="stylesheet" href="{{asset('img')}}"> 
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+    
     <title>Cuenta</title>
-
 </head>
 <body>
       <div class="login-container">
+        <img class="image-container" src="image/844213.png" alt="">
           <div class="login-info-container">
-            <h1 class="title">Registrate</h1>
+            <h1 class="title">Entra aqui</h1>
             <div class="social-login">
                 <div class="social-login-element">
                     <img src="image/google.png" alt="google-image">
@@ -27,15 +27,12 @@
             <p>o</p>
             <form class="inputs-container">
                 <input class="input" type="text" placeholder="Usuario" required>
-                <input class="input" type="email" placeholder="Gmail" require>
                 <input class="input" type="password"placeholder="Contraseña" require>
-                <input class="input" type="password" placeholder="Confirma Contraseña">
                 <p>Olvidaste la contraseña? <span class="span">Click aqui</span></p>
                 <button class="btn">login</button>
-                <p>No tienes una cuenta? <span class="span">Click aqui</span></p>
+                <p>Ya tienes una cuenta? <span class="span">Click aqui</span></p>
             </form>
           </div>
-            <img class="image-container" src="image/registerimage.jpg" alt="">
       </div>
 
 </body>
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //Route::get('/Mercy', CroshetController::class, 'Formulario');
-Route::get('/ShimejiMJ', function () {
+Route::get('/Registro', function () {
     return view('Register');
+ }); 
+ Route::get('/Login', function () {
+    return view('Login');
  }); 
 Route::get('/', function () {
     return view('welcome');
