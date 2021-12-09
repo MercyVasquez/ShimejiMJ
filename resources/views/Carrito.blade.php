c2232b38579820c73b9fdb081fadd9c26b1c5e00
HopeGgukie <93332157+HopeGgukie@users.noreply.github.com>
2021-12-09 11:46:57 -0400

    carrito

--- resources/views/Carrito.blade.php
+++ resources/views/Carrito.blade.php
@@ -40,15 +40,6 @@
                         </li>
                     </ul>
                 </div>
-                <div class="row">
-                    <div class="four columns">
-                        <ul class="navct">
-                            <li class="four columns "><a href="#">Inicio</a></li>
-                            <li class="four columns "><a href="#">Sobre</a></li>
-                            <li class="four columns "><a href="#">Contacto</a></li>
-                        </ul>
-                    </div>
-                </div>
             </div>
         </div>
     </header>
@@ -102,10 +93,10 @@
             <div class="card">
                 <img src="" class="u-full-width">
                 <div class="info-card">
-                    <h4>Yellow Tanabe</h4>
-                    <p>Especial navidad</p>
+                    <h4>Steel Ball Run</h4>
+                    <p>Jojo´s Bizarre Adventure:Steel Ball Run, Vol 1, EN ESPAÑOL</p>
                     <img src="" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,720.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -116,10 +107,10 @@
             <div class="card">
                 <img src="img/bizcocho.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Bizcocho de vainilla</h4>
-                    <p>Básico</p>
+                    <h4>Your Name</h4>
+                    <p>Your Name, Vol 1, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,485.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -130,10 +121,10 @@
             <div class="card">
                 <img src="img/brownies.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Brownies Oreo</h4>
-                    <p>Extra dulce</p>
+                    <h4>Banana Fish</h4>
+                    <p>Banana Fish, Vol 1, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$2,000.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -147,10 +138,10 @@
             <div class="card">
                 <img src="img/brazo_gitano.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Brazo gitano</h4>
-                    <p>Básico</p>
+                    <h4>Parasite</h4>
+                    <p>neo Parasite, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,600.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -161,10 +152,10 @@
             <div class="card">
                 <img src="img/cupcakes.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Christmas cupcakes</h4>
-                    <p>Especial navidad</p>
+                    <h4>Naruto</h4>
+                    <p>Naruto, Vol.69, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,335.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -175,10 +166,10 @@
             <div class="card">
                 <img src="img/tres_leches.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Tres leches</h4>
-                    <p>Básico</p>
+                    <h4>Kishibe Rohan</h4>
+                    <p>Kishibe Rohan Vol.1, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,580.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -192,10 +183,10 @@
             <div class="card">
                 <img src="img/brwoniestoppings.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Brownies toppings</h4>
-                    <p>Extra dulce</p>
+                    <h4>Death Note</h4>
+                    <p>Death Note Vol.1, EN INGLES</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,275.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -206,10 +197,10 @@
             <div class="card">
                 <img src="img/pinkcupcakes.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Pink cupkes</h4>
-                    <p>Extra dulce</p>
+                    <h4>Black Clover</h4>
+                    <p>Black Clover, EN INGLES</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,295.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
@@ -220,62 +211,17 @@
             <div class="card">
                 <img src="img/heartcoockies.jpeg" class="u-full-width">
                 <div class="info-card">
-                    <h4>Pink hearts cookies</h4>
-                    <p>Básico</p>
+                    <h4>Mirai Nikki</h4>
+                    <p>Mirai Nikki, EN ESPAÑOL</p>
                     <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
+                    <p class="precio"><span class="u-pull-right">$1,450.00</span></p>
                     <a href="#" 
                     class="u-full-width button-primary button input agregar-carrito                        
                     ">Agregar Al Carrito</a>
                 </div>
             </div>
         </div>
-    </div><!--.row-->
-
-    <div class="row">
-        <div class="four columns">
-            <div class="card">
-                <img src="img/letteringcoockies.jpeg" class="u-full-width">
-                <div class="info-card">
-                    <h4>Lettering cookies</h4>
-                    <p>Extra dulce</p>
-                    <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
-                    <a href="#" 
-                    class="u-full-width button-primary button input agregar-carrito                        
-                    ">Agregar Al Carrito</a>
-                </div>
-            </div>
-        </div>
-        <div class="four columns">
-            <div class="card">
-                <img src="img/mixedcake.jpeg" class="u-full-width">
-                <div class="info-card">
-                    <h4>Mixed cake</h4>
-                    <p>Extra dulce</p>
-                    <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
-                    <a href="#" 
-                    class="u-full-width button-primary button input agregar-carrito                        
-                    ">Agregar Al Carrito</a>
-                </div>
-            </div>
-        </div>
-        <div class="four columns">
-            <div class="card">
-                <img src="img/butterflycupcakes.jpeg" class="u-full-width">
-                <div class="info-card">
-                    <h4>Butterfly cupcakes</h4>
-                    <p>Extra dulce</p>
-                    <img src="img/estrellas.png" >
-                    <p class="precio">$200 <span class="u-pull-right">$15</span></p>
-                    <a href="#" 
-                    class="u-full-width button-primary button input agregar-carrito                        
-                    ">Agregar Al Carrito</a>
-                </div>
-            </div>
-        </div>
-    </div><!--.row-->
+    </div><!--.row-->               
 </div>
 </body>
 <script src="js/app.js"></script>
